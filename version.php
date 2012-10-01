6e142af7cac180d8407f1fe7a9e3c69d74a720b4
patrickpollet <pp@patrickpollet.net>
2012-10-01 10:50:51 +0200

    added  requires and maturity to version.php

--- version.php
+++ version.php
@@ -5,7 +5,10 @@ defined('MOODLE_INTERNAL') || die();
 // don't forget to bump this in case of change in local/db ...
 $plugin->version   = 2012012415;        // The current plugin version (Date: YYYYMMDDXX)
 
-$plugin->requires  = 2011112900;        // Requires this Moodle version (Moodle 2.0 ) 
+$plugin->requires  = 2012062500; // Requires this Moodle version 2.3
 $plugin->component = 'local_mass_enroll';       // Full name of the plugin (used for diagnostics)
+$plugin->component = 'tool_uploaduser'; // Full name of the plugin (used for diagnostics)
+$plugin->maturity = MATURITY_STABLE; // required for registering to Moodle's database of plugins 
+
 
 ?>
