7e163513744203d06886ce3bf54ca18d84814797
patrickpollet <pp@patrickpollet.net>
2012-10-01 11:13:23 +0200

    added  requires and maturity to version.php III

--- version.php
+++ version.php
@@ -8,4 +8,4 @@ $plugin->version   = 2012012415;        // The current plugin version (Date: YYY
 $plugin->requires  = 2012062500; // Requires this Moodle version 2.3
 $plugin->component = 'local_mass_enroll';       // Full name of the plugin (used for diagnostics)
 $plugin->maturity = MATURITY_STABLE; // required for registering to Moodle's database of plugins 
-
+$plugin->release =  '2.3 (Build 20121001)'; // required for registering to Moodle's database of plugins
