1443d4ef12a32a1ccdd40290c3051d214e4ed7c6
patrickpollet <pp@patrickpollet.net>
2012-10-11 11:51:46 +0200

    in M22 branch version should be 2011120100

--- version.php
+++ version.php
@@ -5,7 +5,7 @@ defined('MOODLE_INTERNAL') || die();
 // don't forget to bump this in case of change in local/db ...
 $plugin->version   = 2012012415;        // The current plugin version (Date: YYYYMMDDXX)
 
-$plugin->requires  = 2012062500; // Requires this Moodle version 2.3
+$plugin->requires  = 2011120100;        // Moodle 2.2 onwards
 $plugin->component = 'local_mass_enroll';       // Full name of the plugin (used for diagnostics)
 $plugin->maturity = MATURITY_STABLE; // required for registering to Moodle's database of plugins 
 $plugin->release = '2.2 (Build 20121001)';  // required for registering to Moodle's database of plugins
